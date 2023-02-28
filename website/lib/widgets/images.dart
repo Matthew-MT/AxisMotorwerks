fda0cf679bc836e6ebc0ad2fc301c5378e22d00d
Lord-Chaos-YT <thumper540@gmail.com>
2023-02-28 14:08:35 -0700

    Got rid of warning messages, reformatted files to conform to vscode's auto-formatting

--- website/lib/widgets/images.dart
+++ website/lib/widgets/images.dart
@@ -1,20 +1,19 @@
 import 'package:flutter/material.dart';
 
 class ImageHandler extends StatelessWidget {
-    const ImageHandler({super.key});
-    @override
-    Widget build(BuildContext context) {
-        return Scaffold(
-                //appBar: AppBar( title: Text('Axis Motorwerks')),
-                body: Center(
-                    child: Container(
-                        child: Image.asset(
-                            'assets/images/sample_mechanic_image.jpg',
-                            height: 100,
-                            width: 200,
-                            fit: BoxFit.fitWidth,),
-                ),
-            ),
-        );
-    }
+  const ImageHandler({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //appBar: AppBar( title: Text('Axis Motorwerks')),
+      body: Center(
+        child: Image.asset(
+          'assets/images/sample_mechanic_image.jpg',
+          height: 100,
+          width: 200,
+          fit: BoxFit.fitWidth,
+        ),
+      ),
+    );
+  }
 }
--- website/lib/widgets/navigation_bar.dart
+++ website/lib/widgets/navigation_bar.dart
@@ -2,42 +2,51 @@ import 'package:flutter/material.dart';
 import './images.dart';
 
 class HomeNavigationBar extends StatelessWidget {
-    const HomeNavigationBar({super.key});
+  const HomeNavigationBar({super.key});
 
-    @override
-    Widget build(BuildContext context) {
-        return Container(
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 100,
+      child: Row(
+        children: <Widget>[
+          const SizedBox(
             height: 100,
-            child: Row(
-                children: <Widget>[
-                    SizedBox(
-                        height: 100,
-                        width: 200,
-                        child: ImageHandler(),
-                    ),
-                    Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: <Widget>[
-                            SizedBox(width: 60,),
-                            _NavigationItem('FAQ'),
-                            SizedBox(width: 60,),
-                            _NavigationItem('Pricing'),
-                            SizedBox(width: 60,),
-                            _NavigationItem('Contact'),
-                        ],
-                    ),
-                ],
-            ),
-        );
-    }
+            width: 200,
+            child: ImageHandler(),
+          ),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: const <Widget>[
+              SizedBox(
+                width: 60,
+              ),
+              _NavigationItem('FAQ'),
+              SizedBox(
+                width: 60,
+              ),
+              _NavigationItem('Pricing'),
+              SizedBox(
+                width: 60,
+              ),
+              _NavigationItem('Contact'),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
 }
 
 class _NavigationItem extends StatelessWidget {
-    final String title;
-    const _NavigationItem(this.title);
+  final String title;
+  const _NavigationItem(this.title);
 
-    @override
-    Widget build(BuildContext context) {
-        return Text(title, style: TextStyle(fontSize: 18),);
-    }
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      title,
+      style: const TextStyle(fontSize: 18),
+    );
+  }
 }
