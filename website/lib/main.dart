5e1ec92055beb146b0a5fde98c9b36df10e19f4b
ejmccallum <ejmccallum@mavs.coloradomesa.edu>
2023-02-28 08:46:55 -0700

    added navigation bar class

--- website/lib/main.dart
+++ website/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import './images.dart';
+import './widgets/navigation_bar.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +16,10 @@ class MyApp extends StatelessWidget {
       routes: <String, WidgetBuilder>{
         "/": (BuildContext buildContext) {
           return Scaffold(
-            body: ImageHandler(),
+            backgroundColor: Colors.blue,
+            body: Column(children: <Widget>[
+              HomeNavigationBar()
+            ]),
             appBar: AppBar(
               title: const Text("Home"),
             ),
