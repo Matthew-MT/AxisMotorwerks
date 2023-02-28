7bc48d213813650b93e5e730bb43205ce293cf8f
ejmccallum <ejmccallum@mavs.coloradomesa.edu>
2023-02-28 14:14:16 -0700

    added pages

--- website/lib/widgets/images.dart
+++ website/lib/widgets/images.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 class ImageHandler extends StatelessWidget {
-<<<<<<< HEAD
     const ImageHandler({super.key});
     @override
     Widget build(BuildContext context) {
@@ -18,21 +17,3 @@ class ImageHandler extends StatelessWidget {
         );
     }
 }
-=======
-  const ImageHandler({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      //appBar: AppBar( title: Text('Axis Motorwerks')),
-      body: Center(
-        child: Image.asset(
-          'assets/images/sample_mechanic_image.jpg',
-          height: 100,
-          width: 200,
-          fit: BoxFit.fitWidth,
-        ),
-      ),
-    );
-  }
-}
->>>>>>> fda0cf679bc836e6ebc0ad2fc301c5378e22d00d
