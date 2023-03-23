48f2c284f80c7ae06d9a75ce6791fbf102bb3e1f
Lord-Chaos-YT <thumper540@gmail.com>
2023-03-23 11:08:19 -0600

    Fixed formatting

--- website/lib/widgets/appointment.dart
+++ website/lib/widgets/appointment.dart
@@ -1,53 +1,47 @@
 import 'package:flutter/material.dart';
 
-class BookAppointment extends StatefulWidget{
-    const BookAppointment({super.key});
+class BookAppointment extends StatefulWidget {
+  const BookAppointment({super.key});
 
-    final String title = "Book";
+  final String title = "Book";
 
-    @override
-    State<BookAppointment> createState() => _AppointmentState();
+  @override
+  State<BookAppointment> createState() => _AppointmentState();
 }
-
 
 class _AppointmentState extends State<BookAppointment> {
   String _content = '';
   final myController = TextEditingController();
 
-    @override
-    void initState() {
-        super.initState();
-        // Start listening to changes.
-        myController.addListener(_handleChange);
-    }
+  @override
+  void initState() {
+    super.initState();
+    // Start listening to changes.
+    myController.addListener(_handleChange);
+  }
 
-    void _handleChange() {
-        setState(()
-        {
-            _content = myController.text;
-        });
-    }
+  void _handleChange() {
+    setState(() {
+      _content = myController.text;
+    });
+  }
 
-    Widget build(BuildContext context){
-        return Scaffold(
-            body: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                    const Text(
-                        'Book Appointment'
-                    ),
-                    TextField(
-                        onChanged: (String value){
-                            setState(()
-                            {
-                                _content = value;
-                            });
-                        },
-                        autofocus: true,
-                    ),
-                ],
-            ),
-        );
-    }
-
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          const Text('Book Appointment'),
+          TextField(
+            onChanged: (String value) {
+              setState(() {
+                _content = value;
+              });
+            },
+            autofocus: true,
+          ),
+        ],
+      ),
+    );
+  }
 }
--- website/lib/widgets/images.dart
+++ website/lib/widgets/images.dart
@@ -1,19 +1,20 @@
 import 'package:flutter/material.dart';
 
 class ImageHandler extends StatelessWidget {
-    const ImageHandler({super.key});
-    @override
-    Widget build(BuildContext context) {
-        return Scaffold(
-                body: Center(
-                    child: Container(
-                        child: Image.asset(
-                            'assets/images/sample_mechanic_image.jpg',
-                            height: 100,
-                            width: 180,
-                            fit: BoxFit.fitWidth,),
-                ),
-            ),
-        );
-    }
+  const ImageHandler({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          child: Image.asset(
+            'assets/images/sample_mechanic_image.jpg',
+            height: 100,
+            width: 180,
+            fit: BoxFit.fitWidth,
+          ),
+        ),
+      ),
+    );
+  }
 }
