537e18f93108c6500d2582d55925773bbf71ed1e
Lord-Chaos-YT <thumper540@gmail.com>
2023-03-31 22:59:23 -0600

    Banner now works, and it has a parallax effect when screen is too wide.

--- website/lib/main.dart
+++ website/lib/main.dart
@@ -8,11 +8,16 @@ import './pages/contact.dart';
 class PageData {
   final String name;
   final String path;
-  final Widget body;
+  final Widget Function(double) builder;
 
-  PageData({required this.name, required this.path, required this.body});
-  PageData.basic({required this.name, required this.body}) : path = "/${name.toLowerCase()}";
-  PageData.home({required this.body}) : name = "Home", path = "/";
+  PageData({required this.name, required this.path, required Widget body}) : builder = ((double height) => body);
+  PageData.built({required this.name, required this.path, required this.builder});
+
+  PageData.basic({required this.name, required Widget body}) : path = "/${name.toLowerCase()}", builder = ((double height) => body);
+  PageData.basicBuilt({required this.name, required this.builder}) : path = "/${name.toLowerCase()}";
+
+  PageData.home({required Widget body}) : name = "Home", path = "/", builder = ((double height) => body);
+  PageData.homeBuilt({required this.builder}) : name = "Home", path = "/";
 }
 
 void main() {
@@ -29,7 +34,7 @@ class App extends StatefulWidget {
 class _AppState extends State<App> {
   // Add pages here
   final List<PageData> pages = [
-    PageData.home(body: const Homepage()),
+    PageData.homeBuilt(builder: (double appBarHeight) => Homepage(appBarHeight: appBarHeight)),
     PageData.basic(name: "About", body: const About()),
     PageData.basic(name: "Pricing", body: const Pricing()),
     PageData.basic(name: "Contact", body: const Contact()),
@@ -40,41 +45,43 @@ class _AppState extends State<App> {
     return MaterialApp(
       title: 'Books App',
       routes: {for (var pageData in pages) pageData.path: (BuildContext buildContext) {
-        return Scaffold(
-          appBar: AppBar(
-            elevation: 30,
-            flexibleSpace: Container(
-              decoration: const BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                  colors: <Color>[
-                    Color.fromARGB(255, 0, 0, 0),
-                    Color.fromARGB(255, 99, 0, 0),
-                    Color.fromARGB(255, 252, 47, 47),
-                    Color.fromARGB(255, 255, 255, 92),
-                  ],
-                ),
+        final appBar = AppBar(
+          elevation: 30,
+          flexibleSpace: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: <Color>[
+                  Color.fromARGB(255, 0, 0, 0),
+                  Color.fromARGB(255, 99, 0, 0),
+                  Color.fromARGB(255, 252, 47, 47),
+                  Color.fromARGB(255, 255, 255, 92),
+                ],
               ),
             ),
-            leading: MouseRegion(
-              cursor: SystemMouseCursors.click,
-              child: GestureDetector(
-                onTap: () => Navigator.pushNamed(buildContext, "/"),
-                child: Image.asset('images/logo.jpg'),
-              ),
-            ),
-            leadingWidth: 100,
-            actions: [for (var pageData in pages) TextButton(
-              style: TextButton.styleFrom(foregroundColor: Colors.white70),
-              onPressed:
-                ModalRoute.of(buildContext)?.settings.name != pageData.path
-                ? () => Navigator.pushNamed(buildContext, pageData.path)
-                : null,
-              child: Text(pageData.name),
-            )],
           ),
-          body: pageData.body,
+          leading: MouseRegion(
+            cursor: SystemMouseCursors.click,
+            child: GestureDetector(
+              onTap: () => Navigator.pushNamed(buildContext, "/"),
+              child: Image.asset('images/logo.jpg'),
+            ),
+          ),
+          leadingWidth: 100,
+          actions: [for (var pageData in pages) TextButton(
+            style: TextButton.styleFrom(foregroundColor: Colors.white70),
+            onPressed:
+              ModalRoute.of(buildContext)?.settings.name != pageData.path
+              ? () => Navigator.pushNamed(buildContext, pageData.path)
+              : null,
+            child: Text(pageData.name),
+          )],
+        );
+
+        return Scaffold(
+          appBar: appBar,
+          body: pageData.builder(appBar.preferredSize.height),
         );
       }},
     ).animate().fadeIn(duration: 400.ms);
--- website/lib/pages/home.dart
+++ website/lib/pages/home.dart
@@ -1,45 +1,222 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
+import '../utils/utils.dart';
 
 class Homepage extends StatelessWidget {
-  const Homepage({super.key});
+  final double appBarHeight;
+  const Homepage({required this.appBarHeight, super.key});
 
   @override
   Widget build(BuildContext context) {
-    final
-      width = MediaQuery.of(context).size.width;
-    return Container(
-      alignment: Alignment.topLeft,
-      margin: EdgeInsets.only(
-        left: width > 1024 ? (width - 1024) / 4 : 0,
-        right: width > 1024 ? (width - 1024) / 4 : 0,
-      ),
-      padding: EdgeInsets.all(width / 16),
-      decoration: BoxDecoration(
-        color: Colors.lightBlue[50],
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey.withOpacity(0.5),
-            spreadRadius: 5,
-            blurRadius: 7,
-            offset: const Offset(0, 3), // changes position of shadow
-          ),
-        ],
-      ),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: const [
-          Text(
-            "Welcome to Axis Motorwerks!",
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 30,
+    final MediaQueryData query = MediaQuery.of(context);
+    final double width = query.size.width;
+    final double height = query.size.height - appBarHeight;
+    final Image bannerImage = Image.asset(
+      "images/sample_mechanic_image.jpg",
+      width: width,
+      fit: BoxFit.cover,
+    );
+    final controller = ScrollController();
+
+    return FutureBuilder(
+      future: getImageSize(bannerImage),
+      builder: (BuildContext buildContext, AsyncSnapshot<Size> snapshot) {
+        return HomepageContent(
+          bannerImage: bannerImage,
+          appBarHeight: appBarHeight,
+          width: width,
+          height: height,
+          imageSize: snapshot.data,
+          controller: controller
+        );
+      },
+    );
+  }
+}
+
+class HomepageContent extends StatefulWidget {
+  final Image bannerImage;
+  final double appBarHeight;
+  final double width;
+  final double height;
+  final double offset;
+  final Size? imageSize;
+  final ScrollController controller;
+  HomepageContent({
+    required this.bannerImage,
+    required this.appBarHeight,
+    required this.width,
+    required this.height,
+    required this.imageSize,
+    required this.controller,
+    super.key,
+  }) : offset = min((imageSize?.height ?? 0) * (width / (imageSize?.width ?? 1)), height / 3);
+
+  @override
+  State<HomepageContent> createState() => _HomepageContentState();
+}
+
+class _HomepageContentState extends State<HomepageContent> {
+  double bannerTop = 0;
+
+  void listenToScroll() => setState(() {
+    bannerTop =
+      (widget.imageSize?.height ?? 0) * (widget.width / (widget.imageSize?.width ?? 1)) > widget.height
+      ?
+        widget.controller.offset >= widget.offset
+        ? widget.height - (widget.imageSize!.height * (widget.width / (widget.imageSize?.width ?? 1)))
+        : (widget.height - (widget.imageSize!.height * (widget.width / (widget.imageSize?.width ?? 1)))) * (widget.controller.offset / widget.offset)
+      : 0;
+    print("bannerTop: $bannerTop");
+  });
+
+  @override
+  _HomepageContentState() {
+    // widget.controller.addListener(listenToScroll);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    widget.controller.addListener(listenToScroll);
+  }
+
+  @override
+  void dispose() {
+    widget.controller.removeListener(listenToScroll);
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Positioned(
+          top: bannerTop,
+          child: widget.bannerImage,
+        ),
+        SingleChildScrollView(
+          controller: widget.controller,
+          child: Container(
+            alignment: Alignment.topLeft,
+            constraints: BoxConstraints(minHeight: widget.height - widget.offset),
+            margin: EdgeInsets.only(
+              top: widget.offset,
+              left: widget.width > 1024 ? (widget.width - 1024) / 4 : 0,
+              right: widget.width > 1024 ? (widget.width - 1024) / 4 : 0,
+            ),
+            padding: EdgeInsets.all(widget.width / 16),
+            decoration: BoxDecoration(
+              color: Colors.lightBlue[50],
+              boxShadow: const [
+                BoxShadow(
+                  color: Colors.black54,
+                  spreadRadius: 5,
+                  blurRadius: 7,
+                  offset: Offset(0, 3), // changes position of shadow
+                ),
+              ],
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                Text(
+                  "Welcome to Axis Motorwerks!",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30,
+                  ),
+                ),
+                Divider(),
+                Text(
+"""
+Ipsam minus facere dolorem. Exercitationem consequatur illum officia culpa delectus quisquam in veniam. Reprehenderit doloribus qui iure et et fugiat consequatur nam.
+
+Reiciendis aut corrupti rem. Voluptas fugit error a veniam nobis impedit. Ut aut molestias reiciendis saepe non quos aut soluta. Vel qui delectus eveniet velit ea veniam dolores. Sit esse corrupti aut eligendi ab. Ut minima voluptatem harum qui optio.
+
+Quod dolore sed totam quis. Exercitationem dolorem sit alias repellat quisquam tempora. Iste quisquam est quisquam.
+
+Dignissimos quaerat perspiciatis iusto incidunt excepturi assumenda qui velit. Velit atque optio earum. Ut nesciunt adipisci tempora voluptatem omnis sint eos quisquam. Consequatur alias corrupti qui dolorem quia dolor illum ratione. Eius et culpa quasi.
+
+Consequatur quis qui voluptatem nulla aut. Quae labore quia nihil quisquam incidunt beatae. Exercitationem odio in similique consequatur molestiae amet dolorem iusto. Consectetur nam aut voluptatem. Ab voluptatem voluptas ducimus eius qui et.
+
+Ipsam minus facere dolorem. Exercitationem consequatur illum officia culpa delectus quisquam in veniam. Reprehenderit doloribus qui iure et et fugiat consequatur nam.
+
+Reiciendis aut corrupti rem. Voluptas fugit error a veniam nobis impedit. Ut aut molestias reiciendis saepe non quos aut soluta. Vel qui delectus eveniet velit ea veniam dolores. Sit esse corrupti aut eligendi ab. Ut minima voluptatem harum qui optio.
+
+Quod dolore sed totam quis. Exercitationem dolorem sit alias repellat quisquam tempora. Iste quisquam est quisquam.
+
+Dignissimos quaerat perspiciatis iusto incidunt excepturi assumenda qui velit. Velit atque optio earum. Ut nesciunt adipisci tempora voluptatem omnis sint eos quisquam. Consequatur alias corrupti qui dolorem quia dolor illum ratione. Eius et culpa quasi.
+
+Consequatur quis qui voluptatem nulla aut. Quae labore quia nihil quisquam incidunt beatae. Exercitationem odio in similique consequatur molestiae amet dolorem iusto. Consectetur nam aut voluptatem. Ab voluptatem voluptas ducimus eius qui et.
+
+Ipsam minus facere dolorem. Exercitationem consequatur illum officia culpa delectus quisquam in veniam. Reprehenderit doloribus qui iure et et fugiat consequatur nam.
+
+Reiciendis aut corrupti rem. Voluptas fugit error a veniam nobis impedit. Ut aut molestias reiciendis saepe non quos aut soluta. Vel qui delectus eveniet velit ea veniam dolores. Sit esse corrupti aut eligendi ab. Ut minima voluptatem harum qui optio.
+
+Quod dolore sed totam quis. Exercitationem dolorem sit alias repellat quisquam tempora. Iste quisquam est quisquam.
+
+Dignissimos quaerat perspiciatis iusto incidunt excepturi assumenda qui velit. Velit atque optio earum. Ut nesciunt adipisci tempora voluptatem omnis sint eos quisquam. Consequatur alias corrupti qui dolorem quia dolor illum ratione. Eius et culpa quasi.
+
+Consequatur quis qui voluptatem nulla aut. Quae labore quia nihil quisquam incidunt beatae. Exercitationem odio in similique consequatur molestiae amet dolorem iusto. Consectetur nam aut voluptatem. Ab voluptatem voluptas ducimus eius qui et.
+
+Ipsam minus facere dolorem. Exercitationem consequatur illum officia culpa delectus quisquam in veniam. Reprehenderit doloribus qui iure et et fugiat consequatur nam.
+
+Reiciendis aut corrupti rem. Voluptas fugit error a veniam nobis impedit. Ut aut molestias reiciendis saepe non quos aut soluta. Vel qui delectus eveniet velit ea veniam dolores. Sit esse corrupti aut eligendi ab. Ut minima voluptatem harum qui optio.
+
+Quod dolore sed totam quis. Exercitationem dolorem sit alias repellat quisquam tempora. Iste quisquam est quisquam.
+
+Dignissimos quaerat perspiciatis iusto incidunt excepturi assumenda qui velit. Velit atque optio earum. Ut nesciunt adipisci tempora voluptatem omnis sint eos quisquam. Consequatur alias corrupti qui dolorem quia dolor illum ratione. Eius et culpa quasi.
+
+Consequatur quis qui voluptatem nulla aut. Quae labore quia nihil quisquam incidunt beatae. Exercitationem odio in similique consequatur molestiae amet dolorem iusto. Consectetur nam aut voluptatem. Ab voluptatem voluptas ducimus eius qui et.
+
+Ipsam minus facere dolorem. Exercitationem consequatur illum officia culpa delectus quisquam in veniam. Reprehenderit doloribus qui iure et et fugiat consequatur nam.
+
+Reiciendis aut corrupti rem. Voluptas fugit error a veniam nobis impedit. Ut aut molestias reiciendis saepe non quos aut soluta. Vel qui delectus eveniet velit ea veniam dolores. Sit esse corrupti aut eligendi ab. Ut minima voluptatem harum qui optio.
+
+Quod dolore sed totam quis. Exercitationem dolorem sit alias repellat quisquam tempora. Iste quisquam est quisquam.
+
+Dignissimos quaerat perspiciatis iusto incidunt excepturi assumenda qui velit. Velit atque optio earum. Ut nesciunt adipisci tempora voluptatem omnis sint eos quisquam. Consequatur alias corrupti qui dolorem quia dolor illum ratione. Eius et culpa quasi.
+
+Consequatur quis qui voluptatem nulla aut. Quae labore quia nihil quisquam incidunt beatae. Exercitationem odio in similique consequatur molestiae amet dolorem iusto. Consectetur nam aut voluptatem. Ab voluptatem voluptas ducimus eius qui et.
+
+Ipsam minus facere dolorem. Exercitationem consequatur illum officia culpa delectus quisquam in veniam. Reprehenderit doloribus qui iure et et fugiat consequatur nam.
+
+Reiciendis aut corrupti rem. Voluptas fugit error a veniam nobis impedit. Ut aut molestias reiciendis saepe non quos aut soluta. Vel qui delectus eveniet velit ea veniam dolores. Sit esse corrupti aut eligendi ab. Ut minima voluptatem harum qui optio.
+
+Quod dolore sed totam quis. Exercitationem dolorem sit alias repellat quisquam tempora. Iste quisquam est quisquam.
+
+Dignissimos quaerat perspiciatis iusto incidunt excepturi assumenda qui velit. Velit atque optio earum. Ut nesciunt adipisci tempora voluptatem omnis sint eos quisquam. Consequatur alias corrupti qui dolorem quia dolor illum ratione. Eius et culpa quasi.
+
+Consequatur quis qui voluptatem nulla aut. Quae labore quia nihil quisquam incidunt beatae. Exercitationem odio in similique consequatur molestiae amet dolorem iusto. Consectetur nam aut voluptatem. Ab voluptatem voluptas ducimus eius qui et.
+
+Ipsam minus facere dolorem. Exercitationem consequatur illum officia culpa delectus quisquam in veniam. Reprehenderit doloribus qui iure et et fugiat consequatur nam.
+
+Reiciendis aut corrupti rem. Voluptas fugit error a veniam nobis impedit. Ut aut molestias reiciendis saepe non quos aut soluta. Vel qui delectus eveniet velit ea veniam dolores. Sit esse corrupti aut eligendi ab. Ut minima voluptatem harum qui optio.
+
+Quod dolore sed totam quis. Exercitationem dolorem sit alias repellat quisquam tempora. Iste quisquam est quisquam.
+
+Dignissimos quaerat perspiciatis iusto incidunt excepturi assumenda qui velit. Velit atque optio earum. Ut nesciunt adipisci tempora voluptatem omnis sint eos quisquam. Consequatur alias corrupti qui dolorem quia dolor illum ratione. Eius et culpa quasi.
+
+Consequatur quis qui voluptatem nulla aut. Quae labore quia nihil quisquam incidunt beatae. Exercitationem odio in similique consequatur molestiae amet dolorem iusto. Consectetur nam aut voluptatem. Ab voluptatem voluptas ducimus eius qui et.
+
+Ipsam minus facere dolorem. Exercitationem consequatur illum officia culpa delectus quisquam in veniam. Reprehenderit doloribus qui iure et et fugiat consequatur nam.
+
+Reiciendis aut corrupti rem. Voluptas fugit error a veniam nobis impedit. Ut aut molestias reiciendis saepe non quos aut soluta. Vel qui delectus eveniet velit ea veniam dolores. Sit esse corrupti aut eligendi ab. Ut minima voluptatem harum qui optio.
+
+Quod dolore sed totam quis. Exercitationem dolorem sit alias repellat quisquam tempora. Iste quisquam est quisquam.
+
+Dignissimos quaerat perspiciatis iusto incidunt excepturi assumenda qui velit. Velit atque optio earum. Ut nesciunt adipisci tempora voluptatem omnis sint eos quisquam. Consequatur alias corrupti qui dolorem quia dolor illum ratione. Eius et culpa quasi.
+
+Consequatur quis qui voluptatem nulla aut. Quae labore quia nihil quisquam incidunt beatae. Exercitationem odio in similique consequatur molestiae amet dolorem iusto. Consectetur nam aut voluptatem. Ab voluptatem voluptas ducimus eius qui et.
+"""
+                ),
+              ]
             ),
           ),
-          Divider(),
-          Text("Lorem ipsum dolor sit amet"),
-        ]
-      ),
+        ),
+      ],
     );
   }
 }
