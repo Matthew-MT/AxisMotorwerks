44488536c444dc9150aa8a73ed370d52ea9a5e45
ejmccallum <ejmccallum@mavs.coloradomesa.edu>
2023-04-01 15:09:37 -0600

    flexible modifications for cross platform users

--- website/lib/main.dart
+++ website/lib/main.dart
@@ -1,19 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
-import './pages/home.dart';
-import './pages/pricing.dart';
-import './pages/about.dart';
-import './pages/contact.dart';
+import 'package:website/pages/page_data.dart';
+import 'responsiveUI/layout.dart';
+import 'responsiveUI/mobile_nav.dart';
+import 'responsiveUI/desktop_nav.dart';
 
-class PageData {
-  final String name;
-  final String path;
-  final Widget body;
-
-  PageData({required this.name, required this.path, required this.body});
-  PageData.basic({required this.name, required this.body}) : path = "/${name.toLowerCase()}";
-  PageData.home({required this.body}) : name = "Home", path = "/";
-}
+GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
 
 void main() {
   runApp(const App());
@@ -27,13 +19,6 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
-  // Add pages here
-  final List<PageData> pages = [
-    PageData.home(body: const Homepage()),
-    PageData.basic(name: "About", body: const About()),
-    PageData.basic(name: "Pricing", body: const Pricing()),
-    PageData.basic(name: "Contact", body: const Contact()),
-  ];
 
   @override
   Widget build(BuildContext context) {
@@ -41,40 +26,13 @@ class _AppState extends State<App> {
       title: 'Books App',
       routes: {for (var pageData in pages) pageData.path: (BuildContext buildContext) {
         return Scaffold(
-          appBar: AppBar(
-            elevation: 30,
-            flexibleSpace: Container(
-              decoration: const BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                  colors: <Color>[
-                    Color.fromARGB(255, 0, 0, 0),
-                    Color.fromARGB(255, 99, 0, 0),
-                    Color.fromARGB(255, 252, 47, 47),
-                    Color.fromARGB(255, 255, 255, 92),
-                  ],
-                ),
-              ),
-            ),
-            leading: MouseRegion(
-              cursor: SystemMouseCursors.click,
-              child: GestureDetector(
-                onTap: () => Navigator.pushNamed(buildContext, "/"),
-                child: Image.asset('images/logo.jpg'),
-              ),
-            ),
-            leadingWidth: 100,
-            actions: [for (var pageData in pages) TextButton(
-              style: TextButton.styleFrom(foregroundColor: Colors.white70),
-              onPressed:
-                ModalRoute.of(buildContext)?.settings.name != pageData.path
-                ? () => Navigator.pushNamed(buildContext, pageData.path)
-                : null,
-              child: Text(pageData.name),
-            )],
+          key: scaffoldKey,
+          appBar: ResponsiveNavigationBar(
+            mobileNav: mobileAppbar(buildContext, scaffoldKey),
+            desktopNav: desktopAppbar(buildContext),
           ),
           body: pageData.body,
+          drawer: mobileDrawer(buildContext),
         );
       }},
     ).animate().fadeIn(duration: 400.ms);
